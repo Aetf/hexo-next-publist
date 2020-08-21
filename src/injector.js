8b0ed6f187249cc2054f6c49cd299c07bdbef0ba
Aetf <aetf@unlimited-code.works>
2020-08-21 03:44:24 -0400

    Fix injected partial in incorrectly cached

--- src/injector.js
+++ src/injector.js
@@ -4,8 +4,8 @@ const pathFn = require('path');
 const { TEMPLATE_DIR } = require('./consts');
 
 function inject(injects) {
-    injects.head.file('publist-headend', pathFn.join(TEMPLATE_DIR, 'headend.njk'), {}, { cache: true });
-    injects.bodyEnd.file('publist-bodyend', pathFn.join(TEMPLATE_DIR, 'bodyend.njk'), {}, { cache: true });
+    injects.head.file('publist-headend', pathFn.join(TEMPLATE_DIR, 'headend.njk'), {}, { cache: false });
+    injects.bodyEnd.file('publist-bodyend', pathFn.join(TEMPLATE_DIR, 'bodyend.njk'), {}, { cache: false });
 }
 
 module.exports = ctx => {
